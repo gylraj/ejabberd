31d07aefb969dca6e0410f8ed4294fc65f94129f
Gian Olivar <gianolivar@gmail.com>
2018-02-26 22:19:28 +0800

    createRoom

--- src/Traits/Service.php
+++ src/Traits/Service.php
@@ -19,4 +19,35 @@ trait Service
 
         return $response == 0;
     }
+    
+
+    public function createRoom($name, $service)
+    {
+        $response = $this->sendRequest(
+            'create_room',
+            [
+                "name" => $name,
+                "service" => $service,
+                "host" => $this->host
+            ]
+        );
+
+        return $response == 0;
+    }
+
+
+    public function createRoomWithOpts($name, $service, $options = [])
+    {
+        $response = $this->sendRequest(
+            'create_room',
+            [
+                'name' => $name,
+                'service' => $service,
+                'host' => $this->config['domain'],
+                'options' => $options
+            ]
+        );
+
+        return $response == 0;
+    }
 }
--- src/Traits/User.php
+++ src/Traits/User.php
@@ -5,7 +5,7 @@ namespace Ejabberd\Rest\Traits;
 trait User
 {
 
-    /**
+   /**
      * Check if ejabberd user account already exists.
      *
      * @param string $user
@@ -40,6 +40,68 @@ trait User
             [
                 "user" => $accountName,
                 "password" => $password,
+                "host" => $this->host
+            ]
+        );
+
+        return $response == 0;
+    }
+    public function archiveGet($accountName, $password)
+    {
+        $response = $this->sendRequest(
+            'archive_get',
+            [
+                // "user" => $accountName,
+                // "password" => $password,
+                // "host" => $this->host
+            ]
+        );
+
+        return $response == 0;
+    }
+
+
+    /**
+     * Send a message
+     *
+     * @param string $accountName
+     * @param $password
+     * @return mixed
+     * @internal param string $param
+     */
+    public function sendMessage($type, $from, $to, $subject, $body)
+    {
+        $response = $this->sendRequest(
+            'send_message',
+            [
+                "type" => $type,
+                "from" => $from,
+                "to" => $to,
+                "subject" => $subject,
+                "body" => $body
+            ]
+        );
+
+        return $response == 0;
+    }
+    public function numActiveUsers($days)
+    {
+        $response = $this->sendRequest(
+            'num_active_users',
+            [
+                'days' => $days,
+                "host" => $this->host
+            ]
+        );
+
+        return $response == 0;
+    }
+    public function numResources($user)
+    {
+        $response = $this->sendRequest(
+            'num_resources',
+            [
+                'user' => $user,
                 "host" => $this->host
             ]
         );
